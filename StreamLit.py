9754aef7c595a8f7a6ded521ef3d76679b10facb
JesseDanielMark <jesse@jiph.co>
2024-05-22 21:04:45 -0700

    efficiency

--- StreamLit.py
+++ StreamLit.py
@@ -1,42 +1,32 @@
 import streamlit as st
 import math
 
-types = [1,5,10,25]
-d = {} # stores tuples of the form (# of types, [coin list])
+types = [25, 10, 5, 1]
 
 total_sales = 5.43
 cash_in = 6
 
-
 # finds the minimum # of types needed to
 # make change for some number of cents
 def get_coins(cash, sale):
+    numCoins = { "Quarters": 0, "Nickels": 0, "Dimes": 0, "Cents": 0 }
+    totalCoins = 0
+    cents = change
+    
     cash *= 100
     sale *= 100
 
     change = cash - sale
 
-    cents = change
-    
-    numQuarters = math.floor(cents/types[3])
+    while(cents > 0):
 
-    cents -= numQuarters * types[3]
+        for x in range(0,3):
+            num = math.floor(cents/types[x])
+            cents -= num * types[x]
 
-    if(cents > 0):
-        numDimes = math.floor(cents/types[2])
-        cents -= numDimes * types[2]
-    
-    if(cents > 0):
-        numNickels = math.floor(cents/types[1])
-        cents -= numNickels * types[1]
+            numCoins[0] = num
+            totalCoins += num
 
-    if(cents > 0):
-        numCents = math.floor(cents/types[0])
-        cents -= numCents * types[0]
-    
-    numCoins = [ numCents, numNickels, numDimes, numQuarters ]
-
-    totalCoins = numCents + numNickels + numDimes + numQuarters
 
     return totalCoins, numCoins, change
 
