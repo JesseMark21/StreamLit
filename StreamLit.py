0baa653acf1ddf4470e3c827e8e741aed7527f56
Jesse Mark <48182065+JesseMark21@users.noreply.github.com>
2024-05-22 19:52:59 -0700

    Update StreamLit.py
    updates

--- StreamLit.py
+++ StreamLit.py
@@ -8,16 +8,18 @@ cash_in = 6
 # finds the minimum # of types needed to
 # make change for some number of cents
 def get_coins(cash, sale):
-    cash
+    cash *= 100
+    sale *= 100
+    cents = cash - sale
     if cents in d.keys():
         return d[cents]
-    elif cents > 0:
-        choices = [(m(cents - x)[0] + 1, m(cents - x)[1] + [x]) for x in types if cents >= x]
+    # elif cents > 0:
+    #     choices = [(m(cents - x)[0] + 1, m(cents - x)[1] + [x]) for x in types if cents >= x]
 
-        # given a list of tuples, python's min function
-        # uses the first element of each tuple for comparison
-        d[cents] = min(choices)
-        return d[cents]
+    #     # given a list of tuples, python's min function
+    #     # uses the first element of each tuple for comparison
+    #     d[cents] = min(choices)
+    #     return d[cents]
     else:
         d[0] = (0, [])
         return d[0]
